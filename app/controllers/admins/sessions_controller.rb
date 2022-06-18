bf4aefd17ea5f3983eed667bad82907097a0ff0f
fujitacoma <john.vsvk.0424@gmail.com>
2022-06-18 23:15:58 +0900

    管理人ログイン画面リンク設定

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -21,7 +21,7 @@ class Admins::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    admin_items_path
+    root_path
   end
 
   def after_sign_out_path_for(resource)
