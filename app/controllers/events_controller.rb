9861a3c519f69ac0a2a08ffd9f092ca7cfd7100d
fujitacoma <john.vsvk.0424@gmail.com>
2022-06-21 16:14:36 +0900

    編集・削除機能の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
@@ -12,7 +13,6 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-
     if @event.save
       redirect_to root_path
     else
@@ -21,17 +21,35 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to root_path if @event.destroy
   end
 
   private
 
-  def move_to_index
-    redirect_to root_path unless @event.user == current_user
-  end
-
   def event_params
     params.require(:event).permit(:event_name, :content, :prefecture_id, :date, :official_link, :youtube_link, :instagram_link, :twitter_link, :image).merge(user_id: current_user.id)
+  end
+
+  def find_params
+    @event = Event.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless @event.user == current_user
   end
 
 end
