2881b79b6c4a849327e396f286e09d0df0116cc0
fujitacoma <john.vsvk.0424@gmail.com>
2022-06-23 21:07:50 +0900

    仮実装まで(機能しません。)

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,12 +6,20 @@ class Event < ApplicationRecord
   end
   validates :prefecture_id,      numericality: { other_than: 1, message: "can't be blank" }
 
-
   belongs_to :prefecture
   belongs_to :user
   has_one_attached :image
   has_many :users, through: [:favorites, :comments]
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+
+  def self.search(search)
+    if search != ""
+      Event.where(["event_name like?", "%#{search}%"])
+    else
+      Event.all
+    end
+  end
 
 end
